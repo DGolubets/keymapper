4adc8b7e85931fec5f49da52bedeb891309a0805
Dmitry Golubets <dgolubets@gmail.com>
2016-12-26 19:40:52 +0300

    Fixed error in build.rs

--- build.rs
+++ build.rs
@@ -1,15 +1,22 @@
 use std::env;
 use std::fs;
-use std::io::Write;
 use std::path::Path;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("../../../");
     let res_path = Path::new("resources");
+    let target_resources_path = dest_path.join("resources");
 
-    fs::create_dir(dest_path.join("resources"));
-    fs::copy(res_path.join("log.toml"), dest_path.join("resources/log.toml"));
-    fs::copy(res_path.join("application.conf"), dest_path.join("resources/application.conf"));
-    fs::copy(res_path.join("profiles.xml"), dest_path.join("resources/profiles.xml"));
+    if target_resources_path.exists() {
+        fs::remove_dir_all(&target_resources_path).expect("Failed to remove resource directory");
+    }
+
+    fs::create_dir(&target_resources_path).expect("Failed to create resource directory");
+
+    let files_to_copy = vec!["log.toml", "application.conf", "profiles.xml"];
+
+    for file_name in files_to_copy {
+        fs::copy(res_path.join(file_name), target_resources_path.join(file_name)).unwrap();
+    }
 }
